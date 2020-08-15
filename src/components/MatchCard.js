a5ed791ff740f3849ca64d362637a45606fb2486
alan-lam <alanlam00@gmail.com>
2020-08-14 18:02:34 -0700

    change cors url

--- src/components/MatchCard.js
+++ src/components/MatchCard.js
@@ -6,8 +6,8 @@ import MatchObjTable from './MatchObjTable';
 import { summonerSpellMap, runePathMap, keystoneMap, itemMap } from '../data/gameData.js';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
-// const CORS_URL = 'https://cors-anywhere.herokuapp.com/';
-const CORS_URL = '';
+const CORS_URL = 'https://cors-anywhere.herokuapp.com/';
+// const CORS_URL = '';
 const QUEUE_TYPE_URL = 'https://static.developer.riotgames.com/docs/lol/queues.json';
 const MATCH_URL = 'https://na1.api.riotgames.com/lol/match/v4/matches/';
 
