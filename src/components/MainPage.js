1d54d8bf0d42345bdcd5ea9c04f80e5176bdd9b6
alan-lam <alanlam00@gmail.com>
2020-08-16 02:11:50 -0700

    add highlight name button

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Search from './Search';
 import Background from '../images/champion-lillia-spirit-blossom-splash.jpg';
 
@@ -9,6 +9,12 @@ const background = {
 };
 
 const MainPage = ({ searching, onFormSubmit }) => {
+  const ref = useRef();
+  
+  const highlightText = () => {
+    window.getSelection().selectAllChildren(ref.current);
+  };
+  
   return (
     <div className="h-screen bg-cover bg-no-repeat flex justify-center items-center" style={background}>
       <div className="rounded text-white text-center bg-gray-900 bg-opacity-75 pb-4 px-4">
@@ -17,7 +23,7 @@ const MainPage = ({ searching, onFormSubmit }) => {
         <p className="text-xl">League of Legends player profile information</p>
         <p>Patch: {PATCH_VERSION}</p>
         <Search onFormSubmit={onFormSubmit} />
-        <p>To test it out, use my summoner name: Big Tastÿ</p>
+        <p>To test it out, use my summoner name: <button className="bg-gray-300 text-black p-1" ref={ref} onClick={highlightText} >Big Tastÿ</button></p>
         <div className={`ui ${searching === true ? 'active' : ''} inline loader`}></div>
       </div>
     </div>
